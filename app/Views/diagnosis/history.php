5b8d1a81592855e552bf8c146c3e89f754e4dce9
Seman <muhammad.seman030801@gmail.com>
2024-03-31 01:42:58 +0800

    ubah tampilan history

--- app/Views/diagnosis/history.php
+++ app/Views/diagnosis/history.php
@@ -15,9 +15,8 @@
                                     <tr>
                                         <th>#</th>
                                         <th>Penyakit</th>
-                                        <th>Gejala</th>
                                         <th>Kemungkinan</th>
-                                        <th>Tanggal</th>
+                                        <th>Action</th>
                                     </tr>
                                 </thead>
                                 <tbody>
@@ -25,11 +24,38 @@
                                         <tr>
                                             <td><?= $index + 1 ?></td>
                                             <td><?= $history['penyakit'] ?></td>
-                                            <td><?= $history['gejala'] ?></td>
                                             <td><?= $history['nilai'] ?></td>
-                                            <td><?= date('d F Y H:i', strtotime($history['tanggal'])) ?></td>
-
+                                            <td>
+                                                <button class="btn btn-info show-detail-btn" data-toggle="modal" data-target="#detailModal<?= $index; ?>"><i class="bx bx-show"></i> Detail</button>
+                                            </td>
                                         </tr>
+                                        <!-- Modal Detail -->
+                                        <div class="modal fade" id="detailModal<?= $index; ?>" tabindex="-1" role="dialog" aria-labelledby="detailModalLabel<?= $index; ?>" aria-hidden="true">
+                                            <div class="modal-dialog" role="document">
+                                                <div class="modal-content">
+                                                    <div class="modal-header">
+                                                        <h5 class="modal-title" id="detailModalLabel<?= $index; ?>">Detail Diagnosis</h5>
+                                                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                                            <span aria-hidden="true">&times;</span>
+                                                        </button>
+                                                    </div>
+                                                    <div class="modal-body">
+                                                        <div class="form-group">
+                                                            <label for="gejala<?= $index; ?>">Gejala</label>
+                                                            <textarea class="form-control" id="gejala<?= $index; ?>" rows="4" readonly><?= $history['gejala'] ?></textarea>
+                                                        </div>
+                                                        <div class="form-group">
+                                                            <label for="tanggal<?= $index; ?>">Tanggal</label>
+                                                            <input type="text" class="form-control" id="tanggal<?= $index; ?>" value="<?= date('d F Y H:i', strtotime($history['tanggal'])) ?>" readonly>
+                                                        </div>
+                                                    </div>
+                                                    <div class="modal-footer">
+                                                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <!-- /Modal Detail -->
                                     <?php endforeach; ?>
                                 </tbody>
                             </table>
@@ -41,4 +67,5 @@
     </div>
 </div>
 <!-- / Content -->
+
 <?= $this->endSection() ?>
